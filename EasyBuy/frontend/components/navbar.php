3a469c68db6cacea7e6a360afbd3015f897005bf
jamespatrickkk <orlanesjamespatrick@gmail.com>
2025-12-19 21:07:24 +0800

    updating cart.php

--- EasyBuy/frontend/components/navbar.php
+++ EasyBuy/frontend/components/navbar.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous" />
-    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-    <link rel="stylesheet" href="../assets/css/style.css" />
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg" style="background: var(--gradient-color)">
+<nav class="navbar navbar-expand-lg" style="background: var(--gradient-color)">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
                 <img src="../assets/navbar_logo.svg" alt="" class="img-fluid px-lg-5 p-2" style="max-height: 68px;">
@@ -70,10 +56,3 @@
             </div>
         </div>
     </nav>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
