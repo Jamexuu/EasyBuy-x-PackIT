cdfd8498da9a977dcc3ce0744fb7a61c6e4267b0
Jamexuu <jamesmercado2209@gmail.com>
2025-12-19 19:29:05 +0800

    create footer

---
+++ EasyBuy/frontend/components/footer.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+    <link rel="stylesheet" href="../../assets/css/style.css" />
+</head>
+
+<body>
+    <div class="container-fluid" style="background: var(--gradient-color-footer);">
+        <footer class="py-3 px-5">
+            <div class="row">
+                <div class="col-3">
+                    <div class="row py-2">
+                        <div class="col">
+                            <img src="../../assets/navbar_logo.svg" alt="" class="img-fluid">
+                        </div>
+                    </div>
+                    <div class="row my-2">
+                        <div class="col">
+                            <ul class="list-unstyled fw-normal">
+                                <li>
+                                    <a href="" class="text-dark text-decoration-none">
+                                        <span class="material-symbols-outlined">
+                                            location_on
+                                        </span>
+                                        Tanauan City, Batangas, Philippines
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" class="text-dark text-decoration-none">
+                                        <span class="material-symbols-outlined">
+                                            phone_in_talk
+                                        </span>
+                                        09455678910
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="" class="text-dark text-decoration-none">
+                                        <span class="material-symbols-outlined">
+                                            mail
+                                        </span>
+                                        info@easybuy.com
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-2 py-5">
+                    <ul class="list-unstyled fw-normal">
+                        <li>
+                            <div class="h6 fw-bold  ">Company</div>
+                        </li>
+                        <li>
+                            <a href="" class="text-decoration-none text-dark">About</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-decoration-none text-dark">Location Map</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-decoration-none text-dark">About</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-2 py-5">
+                    <ul class="list-unstyled fw-normal">
+                        <li>
+                            <div class="h6 fw-bold">Services</div>
+                        </li>
+                        <li>
+                            <a href="" class="text-decoration-none text-dark">My Account</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-decoration-none text-dark">Terms & Conditions</a>
+                        </li>
+                        <br>
+                        <li>
+                            <div class="h6 fw-bold">We Accept</div>
+                        </li>
+                        <li>
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/PayPal.svg/2560px-PayPal.svg.png" alt="Paypal Logo" class="img-fluid p-3">
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-2 py-5">
+                    <ul class="list-unstyled fw-normal">
+                        <li>
+                            <div class="h6 fw-bold">Customer Care</div>
+                        </li>
+                        <li>
+                            <a href="" class="text-decoration-none text-dark">FAQ</a>
+                        </li>
+                        <li>
+                            <a href="" class="text-decoration-none text-dark">Contact us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-3 py-5">
+                    <div class="h6 fw-bold">Newsletter</div>
+                    <div class=" fw-normal">Subscribe to our weekly Newsletter and
+                        receive updates via SMS.</div>
+                    <br>
+                    <div class="row">
+                        <div class="col-10">
+                            <input type="text" placeholder="Phone Number" class="form-control rounded-3">
+                        </div>
+                        <div class="col-2 d-flex align-items-center">
+                            <span class="material-symbols-outlined">
+                                send
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
