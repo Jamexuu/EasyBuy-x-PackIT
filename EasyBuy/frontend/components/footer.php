f17e4fd8e0e91c8eca85c634c36ebd20090cb011
abigailcbarrion <abigailcbarrion@iskolarngbayan.pup.edu.ph>
2025-12-22 02:03:34 +0800

    changes to navbar and footer, added indicators on carousel

--- EasyBuy/frontend/components/footer.php
+++ EasyBuy/frontend/components/footer.php
@@ -1,37 +1,37 @@
-<div class="container-fluid" style="background: var(--gradient-color-footer);">
-  <footer class="py-3 px-5">
+<div id="footerWrapper" class="container-fluid" style="background: var(--gradient-color-footer);">
+  <footer class="py-3 px-5" style="background: transparent;">
     <div class="row">
       <div class="col-12 col-lg-3">
         <div class="row py-2">
           <div class="col">
-            <img src="../assets/navbar_logo.svg" alt="" class="img-fluid">
+            <img src="assets/navbar_logo.svg" alt="" class="img-fluid">
           </div>
         </div>
         <div class="row my-2">
           <div class="col">
             <ul class="list-unstyled fw-normal">
               <li>
-                <a href="" class="text-dark text-decoration-none">
-                  <span class="material-symbols-outlined">
+                <a href="" class="text-dark text-decoration-none d-flex align-items-center gap-2">
+                  <span class="material-symbols-outlined" style="font-size:20px; line-height:1;">
                     location_on
                   </span>
-                  Tanauan City, Batangas, Philippines
+                  <span>Tanauan City, Batangas</span>
                 </a>
               </li>
               <li>
-                <a href="" class="text-dark text-decoration-none">
-                  <span class="material-symbols-outlined">
+                <a href="" class="text-dark text-decoration-none d-flex align-items-center gap-2">
+                  <span class="material-symbols-outlined" style="font-size:20px; line-height:1;">
                     phone_in_talk
                   </span>
-                  09455678910
+                  <span>09455678910</span>
                 </a>
               </li>
               <li>
-                <a href="" class="text-dark text-decoration-none">
-                  <span class="material-symbols-outlined">
+                <a href="" class="text-dark text-decoration-none d-flex align-items-center gap-2">
+                  <span class="material-symbols-outlined" style="font-size:20px; line-height:1;">
                     mail
                   </span>
-                  info@easybuy.com
+                  <span>info@easybuy.com</span>
                 </a>
               </li>
             </ul>
@@ -70,7 +70,8 @@
             <div class="h6 fw-bold">We Accept</div>
           </li>
           <li>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/PayPal.svg/2560px-PayPal.svg.png" alt="Paypal Logo" class="img-fluid p-3">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/PayPal.svg/2560px-PayPal.svg.png" alt="Paypal Logo" class="img-fluid p-3"
+              style="max-height:60px;">
           </li>
         </ul>
       </div>
@@ -86,26 +87,23 @@
             <a href="" class="text-decoration-none text-dark">Contact us</a>
           </li>
         </ul>
+
       </div>
-      <div class="col-12 col-lg-3 py-lg-5">
+      <div class="col-12 col-lg-3 pb-5 mb-5 py-lg-5">
         <div class="h6 fw-bold">Newsletter</div>
-        <div class=" fw-normal">Subscribe to our weekly Newsletter and
-          receive updates via SMS.</div>
+        <div class="fw-normal">Subscribe to our weekly Newsletter and receive updates via SMS.</div>
         <br>
-        <div class="row">
-          <div class="col-10">
-            <input type="text" placeholder="Phone Number" class="form-control rounded-3">
-          </div>
-          <div class="col-2 d-flex align-items-center">
-            <span class="material-symbols-outlined">
-              send
-            </span>
-          </div>
+        <div class="d-flex align-items-center gap-2">
+          <input type="text" placeholder="Phone Number" class="form-control rounded-5" style="max-width:250px">
+          <button class="btn" style="border-radius:999px; ">
+            <span class="material-symbols-outlined text-white">send</span>
+          </button>
         </div>
       </div>
     </div>
   </footer>
 </div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
   integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
   crossorigin="anonymous"></script>
