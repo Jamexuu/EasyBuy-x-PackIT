081c9b97229ad5dac5672639f218c4ef371ce89c
abigailcbarrion <abigailcbarrion@iskolarngbayan.pup.edu.ph>
2025-12-22 16:08:47 +0800

    temp saleCarousel included

---
+++ EasyBuy/frontend/components/saleCarousel.php
@@ -0,0 +1,171 @@
+<div class="container-fluid py-3 pt-4 p-2 p-lg-5">
+    <div class="row">
+      <div class="col mb-4 d-flex justify-content-center justify-content-lg-start">
+        <div class="h1 fw-bold text-md-center ps-3 text-lg-start" style="color:#6EC064">
+          Sale
+        </div>
+      </div>
+    </div>
+    <div id="saleCarousel" class="carousel slide" data-bs-ride="carousel">
+      <div id="saleCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="container">
+              <div class="row g-2 g-lg-3 justify-content-center">
+                <div class="col-6 col-lg-3">
+                  <div class="card h-100 text-center shadow-sm rounded-4">
+                    <img src="Product Images/Produce/1.webp"
+                      class="card-img-top p-2 p-lg-4"
+                      alt="Product 1"
+                      style="height:200px; object-fit:contain;">
+                    <div class="card-body p-2 p-lg-3">
+                      <h6 class="card-title mb-1 fs-6 fs-lg-5">Product Name</h6>
+                      <div class="fw-bold mb-2 fs-6 fs-lg-5">₱399.00</div>
+                      <button class="btn btn-sm btn-outline-success rounded-pill mb-3">
+                        ADD TO CART
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <div class="card h-100 text-center shadow-sm rounded-4">
+                    <img src="Product Images/Meats/11.webp"
+                      class="card-img-top p-2 p-lg-4"
+                      alt="Product 1"
+                      style="height:200px; object-fit:contain;">
+                    <div class="card-body p-2 p-lg-3">
+                      <h6 class="card-title mb-1 fs-6 fs-lg-5">Product Name</h6>
+                      <div class="fw-bold mb-2 fs-6 fs-lg-5">₱399.00</div>
+                      <button class="btn btn-sm btn-outline-success rounded-pill mb-3">
+                        ADD TO CART
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <div class="card h-100 text-center shadow-sm rounded-4">
+                    <img src="Product Images/Dairy/21.webp"
+                      class="card-img-top p-2 p-lg-4"
+                      alt="Product 1"
+                      style="height:200px; object-fit:contain;">
+                    <div class="card-body p-2 p-lg-3">
+                      <h6 class="card-title mb-1 fs-6 fs-lg-5">Product Name</h6>
+                      <div class="fw-bold mb-2 fs-6 fs-lg-5">₱399.00</div>
+                      <button class="btn btn-sm btn-outline-success rounded-pill mb-3">
+                        ADD TO CART
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <div class="card h-100 text-center shadow-sm rounded-4">
+                    <img src="Product Images/Frozen/31.webp"
+                      class="card-img-top p-2 p-lg-4"
+                      alt="Product 1"
+                      style="height:200px; object-fit:contain;">
+                    <div class="card-body p-2 p-lg-3">
+                      <h6 class="card-title mb-1 fs-6 fs-lg-5">Product Name</h6>
+                      <div class="fw-bold mb-2 fs-6 fs-lg-5">₱399.00</div>
+                      <button class="btn btn-sm btn-outline-success rounded-pill mb-3">
+                        ADD TO CART
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="container">
+              <div class="row g-2 g-lg-3 justify-content-center">
+                <div class="col-6 col-lg-3">
+                  <div class="card h-100 text-center shadow-sm rounded-4">
+                    <img src="Product Images/Condiments/41.webp"
+                      class="card-img-top p-2 p-lg-4"
+                      alt="Product 1"
+                      style="height:200px; object-fit:contain;">
+                    <div class="card-body p-2 p-lg-3">
+                      <h6 class="card-title mb-1 fs-6 fs-lg-5">Product Name</h6>
+                      <div class="fw-bold mb-2 fs-6 fs-lg-5">₱399.00</div>
+                      <button class="btn btn-sm btn-outline-success rounded-pill mb-3">
+                        ADD TO CART
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <div class="card h-100 text-center shadow-sm rounded-4">
+                    <img src="Product Images/Snacks/51.webp"
+                      class="card-img-top p-2 p-lg-4"
+                      alt="Product 1"
+                      style="height:200px; object-fit:contain;">
+                    <div class="card-body p-2 p-lg-3">
+                      <h6 class="card-title mb-1 fs-6 fs-lg-5">Product Name</h6>
+                      <div class="fw-bold mb-2 fs-6 fs-lg-5">₱399.00</div>
+                      <button class="btn btn-sm btn-outline-success rounded-pill mb-3">
+                        ADD TO CART
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <div class="card h-100 text-center shadow-sm rounded-4">
+                    <img src="Product Images/Beverages/61.webp"
+                      class="card-img-top p-2 p-lg-4"
+                      alt="Product 1"
+                      style="height:200px; object-fit:contain;">
+                    <div class="card-body p-2 p-lg-3">
+                      <h6 class="card-title mb-1 fs-6 fs-lg-5">Product Name</h6>
+                      <div class="fw-bold mb-2 fs-6 fs-lg-5">₱399.00</div>
+                      <button class="btn btn-sm btn-outline-success rounded-pill mb-3">
+                        ADD TO CART
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-6 col-lg-3">
+                  <div class="card h-100 text-center shadow-sm rounded-4">
+                    <img src="Product Images/Personal/71.webp"
+                      class="card-img-top p-2 p-lg-4"
+                      alt="Product 1"
+                      style="height:200px; object-fit:contain;">
+                    <div class="card-body p-2 p-lg-3">
+                      <h6 class="card-title mb-1 fs-6 fs-lg-5">Product Name</h6>
+                      <div class="fw-bold mb-2 fs-6 fs-lg-5">₱399.00</div>
+                      <button class="btn btn-sm btn-outline-success rounded-pill mb-3">
+                        ADD TO CART
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex  justify-content-center gap-3 mt-3">
+        <button class="btn p-0"
+          type="button"
+          data-bs-target="#saleCarousel"
+          data-bs-slide="prev">
+          <span class="material-symbols-rounded"
+            aria-hidden="true"
+            style="font-size:32px; color:#6EC064;">
+            chevron_left
+          </span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="btn p-0"
+          type="button"
+          data-bs-target="#saleCarousel"
+          data-bs-slide="next">
+          <span class="material-symbols-rounded"
+            aria-hidden="true"
+            style="font-size:32px; color:#6EC064;">
+            chevron_right
+          </span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  </div>
