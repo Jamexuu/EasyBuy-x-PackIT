765633882b3b9bd8fc8a6f89f932c1e753b60b4a
Jamexuu <jamesmercado2209@gmail.com>
2025-12-27 01:33:08 +0800

    add Product class and getAllProducts endpoint for product retrieval

---
+++ EasyBuy/api/classes/Product.php
@@ -0,0 +1,35 @@
+<?php
+
+include 'Database.php';
+
+class Product {
+    private $db;
+
+    function __construct() {
+        $this->db = new Database();
+    }
+
+    function getAllProducts() {
+        $query = "SELECT * FROM products";
+
+        $stmt = $this->db->executeQuery($query);
+        $result = mysqli_stmt_get_result($stmt);
+        $data = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        mysqli_stmt_close($stmt);
+
+        return $data;
+    }
+
+    function getProductById($productId) {
+        $query = "SELECT * FROM products WHERE id = ?";
+        $params = [$productId];
+        return $this->db->executeQuery($query, $params);
+    }
+
+    function searchProducts($keyword) {
+        $query = "SELECT * FROM products WHERE product_name LIKE ? OR category LIKE ?";
+        $likeKeyword = '%' . $keyword . '%';
+        $params = [$likeKeyword, $likeKeyword];
+        return $this->db->executeQuery($query, $params);
+    }
+}
