4077d14ac3ba3cfa69457d37303b0b6eb682083c
sijey-CJAA <artuzjrchristopher@gmail.com>
2026-01-02 00:05:53 +0800

    added driver feature

---
+++ PackIT/driver/driver.php
@@ -0,0 +1,20 @@
+<?php
+// miel and chelse
+
+//UI
+// ui ng driver ng packit
+// login.php as driver at signup.php as driver
+// in sign up need ng: name, email, contact number, 
+// (house number, street, subdivision, landmark, province, city, barangay), password
+// type of vehicle: car, motorcycle, van, forward truck
+
+// dashboard.php as driver
+
+// buttons accept booking
+// when accept booking update tracking status.
+
+// view ng mga orders na pwedeng i accept
+// once na i accept, lalabas sa dashboard nya yung details ng order
+
+
+?>
