9570a5ceeb30343bdc19cc88a892d5099522508d
sijey-CJAA <artuzjrchristopher@gmail.com>
2026-01-04 16:32:15 +0800

    segregated vehicles' sql

--- PackIT/api/migrations/vehicles.sql
+++ PackIT/api/migrations/vehicles.sql
@@ -22,17 +22,6 @@ CREATE TABLE IF NOT EXISTS `vehicles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-INSERT INTO `vehicles`
-(`name`, `package_type`, `fare`, `max_kg`, `size_length_m`, `size_width_m`, `size_height_m`, `image_file`)
-VALUES
-('Motorcycle', 'Envelope', 100.00, 20, 0.50, 0.40, 0.50, 'motorcycle.png'),
-('Tricycle', 'Small Box', 150.00, 50, 0.70, 0.50, 0.50, 'tricycle.png'),
-('Sedan', 'Med Box', 200.00, 200, 1.00, 0.60, 0.70, 'sedan.png'),
-('Pick-up Truck', 'Big Box', 250.00, 800, 2.70, 1.50, 0.50, 'pickup.png'),
-('Closed Van', 'Pallet (Perishable)', 300.00, 1000, 2.10, 1.30, 1.30, 'van.png'),
-('Forward Truck', 'Pallet (Non Perishable)', 350.00, 1200, 10.00, 2.40, 2.30, 'forward truck.png');
-
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
