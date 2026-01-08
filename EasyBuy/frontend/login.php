e62d247741e05292c49368edb9c22f4ffb76474e
Denscape <dnmrrdnd@gmail.com>
2026-01-09 03:13:26 +0800

    minimal change

--- EasyBuy/frontend/login.php
+++ EasyBuy/frontend/login.php
@@ -67,7 +67,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                     <div class="h5 fw-bold mb-2">Error</div>
                                     <p class="mb-4"> Invalid Credentials</p>
                                     <div class="d-flex justify-content-center">
-                                        <button type="button" class="btn px-4 text-white" style="background-color: #6EC064;" data-bs-dismiss="modal">
+                                        <button type="button" class="btn px-4 text-white" style="background-color: #6EC064;"
+                                            data-bs-dismiss="modal">
                                             OK
                                         </button>
                                     </div>
@@ -108,7 +109,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     </div>
                     <p class="text-center mb-1 mt-2">
                         Don't have an account?
-                        <a href="signUp.php" class="text-black text-decoration-underline ">Sign Up to
+                        <a href="signUp.php" class="text-black text-decoration-underline ">Sign Up with
                             EasyBuy</a>
                     </p>
                 </form>
