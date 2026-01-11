fcf96001de3035996817fa0b4af6dc57314f6d3b
Jamexuu <jamesmercado2209@gmail.com>
2026-01-11 16:30:00 +0800

    Add error handling for missing vendor autoload file in config

--- EasyBuy/api/config.php
+++ EasyBuy/api/config.php
@@ -1,5 +1,11 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+$vendorPath = __DIR__ . '/../vendor/autoload.php';
+
+if (!file_exists($vendorPath)) {
+    die("Vendor autoload not found at: " . realpath(__DIR__ . '/..') . "/vendor/autoload.php");
+}
+
+require_once $vendorPath;
 
 use Dotenv\Dotenv;
 
