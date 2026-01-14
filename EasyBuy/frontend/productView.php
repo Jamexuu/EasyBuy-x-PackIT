12e992ac3ee64002706ca065a69cefb98b826a1d
Jamexuu <jamesmercado2209@gmail.com>
2026-01-14 23:56:44 +0800

    feat: integrate chatbot component into account, cart, product listing, and product view pages

--- EasyBuy/frontend/productView.php
+++ EasyBuy/frontend/productView.php
@@ -233,7 +233,8 @@
         </div>
     </div>
 
-
+    <?php include 'components/chatbot.php'; ?>
+    <script src="../assets/js/chatbot.js"></script> 
     <script>
         let currentProduct = null;
         let allProducts = [];
